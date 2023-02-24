e531dc336b04925ce450264dda11b1d089d745b4
Tamás Seres <seres.tamas@magicsolutions.ro>
2023-02-24 14:22:48 +0200

    Navbar redesigned

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router-dom";
+import companyLogo from "../../images/company-logo.png";
 
 export default function Navbar() {
     return (
-        <div className="fixed flex top-0 left-0 right-0 h-16 bg-white backdrop-blur-md z-50 shadow-2xl">
-            <nav className="ml-5 flex items-center gap-x-16 text-gray mr-auto text-xl font-medium max-sm:text-sm max-sm:gap-x-10 ">
-                <Link to="/" className="hover:text-midnight-green">
-                    Home
-                </Link>
-                <Link to="/rent" className="hover:text-midnight-green">
-                    Rent a House
-                </Link>
-                <Link to="/favorite" className="hover:text-midnight-green">
-                    Favorite
-                </Link>
-                <Link to="/login" className="hover:text-midnight-green">
-                    Login
-                </Link>
+        <div className="fixed top-0 left-0 right-0 h-30 bg-white backdrop-blur-md z-50 shadow-2xl">
+            <nav className="ml-5 font-semibold text-xl flex items-center">
+                <div className="flex items-center gap-x-14 mr-auto">
+                    <Link to="/" className="hover:text-midnight-green">
+                        <img
+                            src={companyLogo}
+                            className="h-20"
+                            alt="Company logo"
+                        />
+                    </Link>
+                    <Link to="/rent" className="hover:text-gray">
+                        Rent a House
+                    </Link>
+                    <Link to="/favorite" className="hover:text-gray">
+                        Favorite
+                    </Link>
+                </div>
+                <div className="flex items-center mr-16">
+                    <Link to="/login" className="hover:text-gray ">
+                        Login
+                    </Link>
+                </div>
             </nav>
         </div>
     );
