bf047d1de78b76f556ef185963a57627293f9ce3
beczebalazs <balazs7699@gmail.com>
2023-03-14 18:58:58 +0200

    api key

--- src/store/realEstates.action.ts
+++ src/store/realEstates.action.ts
@@ -12,7 +12,12 @@ import axios from "axios";
 export const fetchRealEstates = () => async (dispatch: Dispatch<AnyAction>) => {
     try {
         dispatch(getRealEstatesStart());
-        const response = await axios.get("http://localhost:3001/real-estates");
+        const response = await axios.get("http://localhost:3001/real-estates", {
+            headers: {
+                API_KEY:
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicHJvamVjdCI6InJlYWwtZXN0YXRlLXdlYiIsImlhdCI6MTUxNjIzOTAyMn0.CLcp9GYYCftz3TJf2JHs6-jPlUSmaSPsUsVFbp2c3Ss",
+            },
+        });
         dispatch(getRealEstatesSuccess(response.data));
     } catch (error: unknown) {
         if (error instanceof Error) {
