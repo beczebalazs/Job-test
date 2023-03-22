9929bb59c54970a66f664a24ed0d39b35a706615
beczebalazs <balazs7699@gmail.com>
2023-03-22 19:33:20 +0200

    Tsconfig

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,9 +1,6 @@
-import Navbar from "../../components/common/navbar";
-
 const NotFound = () => {
     return (
         <div>
-            <Navbar />
             <div className="flex items-center justify-center h-screen">
                 <h1 className="text-2xl">Page not found...</h1>
             </div>
