68519b554021032c16bb04e820c075e287077b3d
Tamás Seres <seres.tamas@magicsolutions.ro>
2023-03-24 16:21:08 +0200

    fix: path aliases are now correct

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,16 @@ module.exports = {
   webpack: {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@providers": path.resolve(__dirname, "src/providers"),
+      "@router": path.resolve(__dirname, "src/router"),
+      "@service": path.resolve(__dirname, "src/service"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+
     },
   },
 };
